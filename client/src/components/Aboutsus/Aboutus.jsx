363a8a5168aa5c6f303bd9cb3baf4a542e556159
toyorcee <oluwatoyosiolaniyan@gmail.com>
2024-09-06 07:20:06 +0100

    modal added

--- client/src/components/Aboutsus/Aboutus.jsx
+++ client/src/components/Aboutsus/Aboutus.jsx
@@ -1,29 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "./About.css"
 import AUimg from '../../assets/Color logo with background.png';
+import cac from '../../assets/CAC.PNG';
 
 export default function Aboutus() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
   return (
     <section className="Aboutus py-12 my-2">
     <div className="container mx-auto px-4">
       <h2 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 text-center">ABOUT US</h2>
-      <div className="flex flex-col md:flex-row items-center gap-2">
-        <div className="md:w-1/2 mt-0 my-5 md:mt-8">
+      <div className="aboutuscontainer grid grid-cols-1 md:grid-cols-[70%_30%] gap-4 items-center my-5">
+        <div className="flex flex-col md:flex-row gap-4">
           <img
             src={AUimg}
             alt="About Us"
-            className="AUimg w-full h-auto rounded-lg shadow-lg"
+            className="about-img w-full h-auto rounded-lg shadow-lg"
+          />
+          <img
+            src={cac}
+            alt="CAC Certificate"
+            className="about-img w-full h-auto rounded-lg shadow-lg cursor-pointer"
+            onClick={() => openModal()}
           />
         </div>
-        <div className='md:w-1/2 mt-0 my-5 md:mt-8'>
-          <p className="text-lg text-gray-700 mb-4">
-            We are a passionate team dedicated to bringing you the best fashion experience. Our mission is to deliver high-quality native attires and fabrics that make you look and feel your best.
-          </p>
-          <p className="text-lg text-gray-700">
-            Our commitment to quality and customer satisfaction drives us to continually improve and innovate. Join us on this exciting journey as we explore the world of fashion together.
+        <div>
+          <p className="aboutus-para text-lg text-gray-700 mb-4 font-bold justify-center align-center">
+            We are a passionate team dedicated to bringing you the best fashion experience...
+            Our commitment to quality and customer satisfaction drives us to continually improve and innovate...
           </p>
         </div>
       </div>
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="modal fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center" onClick={() => closeModal()}>
+          <img src={cac} alt="CAC Certificate Full View" className="max-w-full max-h-full" />
+        </div>
+      )}
     </div>
   </section>
   )
