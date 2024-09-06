13554f9ae1aa22e627c0d8c9998e4f6a32bb9f95
toyorcee <oluwatoyosiolaniyan@gmail.com>
2024-09-06 05:46:35 +0100

    new images

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -18,6 +18,9 @@ import img15 from '../../assets/1c.jpg';
 import img16 from '../../assets/IMG_5693.JPG';
 import img17 from '../../assets/IMG_5694.JPG';
 import img18 from '../../assets/IMG_5695.JPG';
+import img19 from '../../assets/IMG_5701.JPG';
+import img20 from '../../assets/IMG_5702.jpeg';
+import img21 from '../../assets/IMG_5703.PNG';
 export default function Category() {
   const [showMore, setShowMore] = useState(false);
 
@@ -40,6 +43,69 @@ export default function Category() {
           <div className="md:slanted-text hidden">
             <span>ADULT CATEGORY</span>
           </div>
+          <div className="p-4 lg:w-1/3">
+            <div className="h-full bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+              <img src={img19} alt="" className="img" />
+              <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+                <span className="text-[#667EEA] mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                  <svg className="svg w-4 h-4 mr-1" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                    <circle cx="12" cy="12" r="3"></circle>
+                  </svg>
+                  1.2K
+                </span>
+                <span className="text-[#667EEA] inline-flex items-center leading-none text-sm">
+                  <svg className="svg w-4 h-4 mr-1" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                  </svg>
+                  18
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="p-4 lg:w-1/3">
+            <div className="h-full bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+              <img src={img20} alt="" className="img" />
+              <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+                <span className="text-[#667EEA] mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                  <svg className="svg w-4 h-4 mr-1" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                    <circle cx="12" cy="12" r="3"></circle>
+                  </svg>
+                  2.5K
+                </span>
+                <span className="text-[#667EEA] inline-flex items-center leading-none text-sm">
+                  <svg className="svg w-4 h-4 mr-1" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                  </svg>
+                  35
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="p-4 lg:w-1/3">
+            <div className="h-full bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+              <img src={img21} alt="" className="img" />
+              <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+                <span className="text-[#667EEA] mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                  <svg className="svg w-4 h-4 mr-1" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                    <circle cx="12" cy="12" r="3"></circle>
+                  </svg>
+                  3.3K
+                </span>
+                <span className="text-[#667EEA] inline-flex items-center leading-none text-sm">
+                  <svg className="svg w-4 h-4 mr-1" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                  </svg>
+                  23
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+                  {/* Hidden Row of Images */}
+      <div className={`extra-images flex flex-wrap -m-4 transition-all duration-500 ease-in-out ${showMore ? 'max-h-full opacity-100' : 'max-h-0 opacity-0 overflow-hidden'}`}>
           <div className="p-4 lg:w-1/3">
             <div className="h-full bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
               <img src={img16} alt="" className="img" />
@@ -100,9 +166,6 @@ export default function Category() {
               </div>
             </div>
           </div>
-        </div>
-                  {/* Hidden Row of Images */}
-                  <div className={`extra-images flex flex-wrap -m-4 transition-all duration-500 ease-in-out ${showMore ? 'max-h-full opacity-100' : 'max-h-0 opacity-0 overflow-hidden'}`}>
 
           <div className="p-4 lg:w-1/3">
             <div className="h-full bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
